803ce39a2e25a0402fd1561b9f7ff039a239350d
Julia Ohorodnichuk <ogorodnicukulia@gmail.com>
2024-09-07 19:31:26 +0300

    feat: images and articles

---
+++ src/json/articles.ts
@@ -0,0 +1,11 @@
+export const articles = [
+    { category: "Singapore", title: "Gardens by the Bay", url: "#" },
+    { category: "Singapore", title: "Marina Bay Sands", url: "#" },
+    { category: "Singapore", title: "Sentosa Island", url: "#" },
+    { category: "Dubai", title: "Burj Khalifa", url: "#" },
+    { category: "Dubai", title: "Palm Jumeirah", url: "#" },
+    { category: "Dubai", title: "Dubai Mall", url: "#" },
+    { category: "Silicon Valley", title: "Apple Park", url: "#" },
+    { category: "Silicon Valley", title: "Googleplex", url: "#" },
+    { category: "Silicon Valley", title: "Stanford University", url: "#" }
+];
